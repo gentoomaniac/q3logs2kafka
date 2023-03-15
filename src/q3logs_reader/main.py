6e11bf80b42d4a2f26110355dba76cb1e05d8129
Marco Siebecke <marco@siebecke.se>
2023-03-15 17:59:06 +0100

    `loaded map` event now creates a uuid that is added to all events
    This is needed to keep the events together and can later be used to
    push the events to a dedicated kafka topic

--- src/q3logs_reader/main.py
+++ src/q3logs_reader/main.py
@@ -3,6 +3,7 @@
 
 import logging
 import json
+import uuid
 import sys
 
 import click
@@ -31,11 +32,15 @@ def cli(verbosity: int):
 
 @cli.command(name='tail')
 @click.option('-c', '--command', help='command to gather logs', type=str, required=True)
-def foobar(command: str):
+def tail(command: str):
 
+    match_id = None
     for line in run_command(command.split()):
         blob = log_line2blob(line)
         if blob:
+            if blob['event'] == "loaded map":
+                match_id = str(uuid.uuid4())
+            blob['id'] = match_id
             log.info(json.dumps(blob))
 
 
